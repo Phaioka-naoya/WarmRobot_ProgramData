f2648c2577fcc95b569413dfe1821533be0f9775
Phaioka-naoya <70026655+Phaioka-naoya@users.noreply.github.com>
2022-09-07 16:38:14 +0900

    multi_acceleration_moveをmulti_moveに改称：加減速が移動時にはマストなので 移動速度を元に，角速度を返す関数travel_to_degree_speedを追加 acceleration_moveをsingle_movieに改称 acceleration_moveにも加減速機能を追加

--- CheckAllLinearActuater/CheckAllLinearActuater.ino
+++ CheckAllLinearActuater/CheckAllLinearActuater.ino
@@ -15,6 +15,7 @@
 //モータドライバとモータの仕様に準じる
 #define T_CL 10         // CLKパルスのLo/Hiレベル応答幅[µs]
 #define ONESTEP_DEG 1.8 // 1ステップ（パルス）あたりの移動角度[degree]
+#define LEAD 1          //ねじリード．ネジが1回転したときに進む距離[mm]
 
 //ユーザ設定<全体節共通>
 #define AC_TIME 400 //加速時間[ms]
@@ -26,12 +27,22 @@
 /*LinearClassのstaicデータメンバの初期化*/
 int LinearClass::t_cl = T_CL;
 double LinearClass::onestep_deg = ONESTEP_DEG;
-double LinearClass::min_travel_speed = MIN_TRAVEL_SPEED;
+double LinearClass::start_travel_speed = MIN_TRAVEL_SPEED;
 int LinearClass::ac_time = AC_TIME;
+int LinearClass::lead = LEAD;
 
+/*
 LinearClass Seg1_Linear(SEG1_DIR, SEG1_CLK, TRAVEL_SPEED, TRAVEL_STEP);
 LinearClass Seg2_Linear(SEG2_DIR, SEG2_CLK, TRAVEL_SPEED, TRAVEL_STEP);
 LinearClass Seg3_Linear(SEG3_DIR, SEG3_CLK, TRAVEL_SPEED, TRAVEL_STEP);
+*/
+
+LinearClass LinearArray[3] ={
+  LinearClass(SEG1_DIR, SEG1_CLK, TRAVEL_SPEED, TRAVEL_STEP),
+  LinearClass(SEG2_DIR, SEG2_CLK, TRAVEL_SPEED, TRAVEL_STEP),
+  LinearClass(SEG3_DIR, SEG3_CLK, TRAVEL_SPEED, TRAVEL_STEP),
+};
+
 
 /*状態遷移*/
 int state;
@@ -44,8 +55,8 @@ void setup()
   delay(2000);
 }
 
-LinearClass LinearArray[] = {Seg1_Linear, Seg2_Linear, Seg3_Linear}; // LinearClassの配列．複数のリニアアクチュエータを同時に利用する際に利用．
-//なんかローカル変数だとエラーが出るのでグローバルに！！
+//LinearClass LinearArray[] = {Seg1_Linear, Seg2_Linear, Seg3_Linear}; // LinearClassの配列．複数のリニアアクチュエータを同時に利用する際に利用．
+
 void loop()
 {
 
@@ -53,20 +64,36 @@ void loop()
   {
   case 1: // 2体節目開脚，3体節目開脚
     Serial.println("state1");
-    digitalWrite(Seg1_Linear.dir_pin, LOW); //伸展方向
-    digitalWrite(Seg2_Linear.dir_pin, LOW); //伸展方向
-    digitalWrite(Seg3_Linear.dir_pin, LOW); //伸展方向
-    multi_acceleration_move(LinearArray, 3);
+    digitalWrite(LinearArray[0].dir_pin, LOW); //伸展方向
+    digitalWrite(LinearArray[1].dir_pin, LOW); //伸展方向
+    digitalWrite(LinearArray[2].dir_pin, LOW); //伸展方向
+    multi_move(LinearArray, 3);
     delay(LINEAR_WAIT_TIME);
     state = 2;
     break;
 
   case 2:                    
     Serial.println("state2");                 // 1体節目伸展
-    digitalWrite(Seg1_Linear.dir_pin, HIGH); //伸展方向
-    digitalWrite(Seg2_Linear.dir_pin, HIGH); //伸展方向
-    digitalWrite(Seg3_Linear.dir_pin, HIGH); //伸展方向
-    multi_acceleration_move(LinearArray, 3);
+    digitalWrite(LinearArray[0].dir_pin, HIGH); //収縮方向
+    digitalWrite(LinearArray[1].dir_pin, HIGH); //収縮方向
+    digitalWrite(LinearArray[2].dir_pin, HIGH); //収縮方向
+    multi_move(LinearArray, 3);
+    delay(LINEAR_WAIT_TIME);
+    state = 3;
+    break;
+
+  case 3:
+    Serial.println("state3");  
+    digitalWrite(LinearArray[0].dir_pin, LOW); //伸展方向
+    LinearArray[0].single_move();
+    delay(LINEAR_WAIT_TIME);
+    state = 4;
+    break;
+  
+  case 4:
+    Serial.println("state4");
+    digitalWrite(LinearArray[0].dir_pin, HIGH); //収縮方向
+    LinearArray[0].single_move();
     delay(LINEAR_WAIT_TIME);
     state = 1;
     break;
